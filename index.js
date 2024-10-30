22825ec0b1a10d2ad3b74b9c8a07183727ddb4cf
Sylvain LAMBERT <git@sylvain.pro>
2024-10-30 16:34:57 +0100

    FEATURE: ui: Added password vertification before deactivation

--- index.js
+++ index.js
@@ -1146,19 +1146,27 @@ document.addEventListener('DOMContentLoaded', function() {
                     const confirmAction = document.getElementById('confirmAction');
                     confirmAction.disabled = true;
                     try {
-                        const response = await fetch('/disable', {
-                            method: 'POST',
-                            headers: {
-                                'Content-Type': 'application/json'
-                            },
-                            body: JSON.stringify({ commonName: certData.id, passphrase: passphrase })
+                        const getPassword = await fetch('/get-password', {
+                            method: 'GET',
+                            headers: { 'Content-Type': 'application/json' }
                         });
-                
-                        if (response.ok) {
-                            loadCertData();
-                            modal.hide();
+                        if (getPassword.ok) {
+                            const password = await getPassword.json();
+                            const response = await fetch('/disable', {
+                                method: 'POST',
+                                headers: { 'Content-Type': 'application/json' },
+                                body: JSON.stringify({ commonName: commonName, password: password.pkiaccess })
+                            });
+                    
+                            if (response.ok) {
+                                loadCertData();
+                                modal.hide();
+                            } else {
+                                showAlert('passphraseAlert');
+                                confirmAction.disabled = false;
+                            }
                         } else {
-                            showAlert('passphraseAlert');
+                            console.error('Passphrase fetching error:', getPassword.statusText);
                         }
                     } catch (error) {
                         console.error('Deactivation error:', error);
